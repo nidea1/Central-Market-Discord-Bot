f2098a441330a583e907c2ab91882f23f6189d50
Doruk ÇORALI <drk.corali@gmail.com>
2023-01-06 14:00:10 +0300

    Updated

--- BDOMarketWaitList.py
+++ BDOMarketWaitList.py
@@ -7,20 +7,57 @@ from datetime import datetime
 def printResults(data):
     # Use the json module to load the string data into a dictionary
     theJSON = json.loads(data)
+    # Created a new dict for our getted datas
+    obj = {"Sıra: ": 0, "Item: ": "", "Fiyat: ": "", "Listeleneceği saat: ": ""}
 
-    # now we can access the contents of the JSON like any other Python object
-    for i in range(len(theJSON)):
-        print(theJSON[i]["name"])
-        timestamp = theJSON[i]["liveAt"]
-        time = datetime.fromtimestamp(timestamp)
-        ("Register Time: ", str(time.hour)+":"+str(time.minute)+":"+str(time.second))
+    # We querying if it's a list. Cuz theJSON has can more data
+    if type(theJSON) == list:
+
+        # i converting list to dict
+        for i in range(len(theJSON)):
+
+            # now we can access the contents of the JSON like any other Python object
+            for k,v in theJSON[i].items():
+
+                if k == "name":
+                    obj["Item: "] = v
+
+                if k == "price":
+                    obj["Fiyat: "] = "{:,d}".format(v)
+
+                if k == "liveAt":
+                    date = datetime.fromtimestamp(v).strftime("%H:%M:%S")
+                    obj["Listeleneceği saat: "] = date
+                    obj["Sıra: "] += 1
+
+            for key,val in obj.items():
+                print(key,val)
+
+    # if theJSON is not list, it has 1 data and it's dict
+    else:
+
+        # now we can access the contents of the JSON like any other Python object
+        for k,v in theJSON.items():
+
+            if k == "name":
+                obj["Item: "] = v
+
+            if k == "price":
+                obj["Fiyat: "] = v
+
+            if k == "liveAt":
+                date = datetime.fromtimestamp(v).strftime("%H:%M:%S")
+                obj["Listeleneceği saat: "] = date
+
+        for key,val in obj.items():
+            print(key,val)
         
 
 def main():
     # define a variable to hold the source URL
     # in this case we'll use the free data feed from the arsha.io
     # this feed lists Black Desert Online(MENA) Market Wait List
-    # if u want use this for other server just change YourServer from "https://api.arsha.io/v2/YourServer/GetWorldMarketWaitList"
+    # if u want use this for other server just change 'YourServer' from "https://api.arsha.io/v2/YourServer/GetWorldMarketWaitList"
     urlData = "https://api.arsha.io/v2/mena/GetWorldMarketWaitList"
 
     # Open the URL and read the data
