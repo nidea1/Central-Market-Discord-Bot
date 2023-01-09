7257218e2f7db9ded807f586ac898eea5d278280
Doruk ÇORALI <drk.corali@gmail.com>
2023-01-09 14:40:05 +0300

    Updated to discord bot

--- BDOMarketWaitList.py
+++ BDOMarketWaitList.py
@@ -1,13 +1,28 @@
-import urllib.request
+import discord
+import requests
 import json
 from datetime import datetime
 
+intents = discord.Intents.default()
+intents.message_content = True
 
-def printResults(data):
+client = discord.Client(intents=intents)
+
+# Client log-in
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+# BDO-MENA Market
+def WaitList():
     # Use the json module to load the string data into a dictionary
-    theJSON = json.loads(data)
+    response = requests.get("https://api.arsha.io/v2/mena/GetWorldMarketWaitList?lang=tr")
+    theJSON = json.loads(response.text)
     # Created a new dict for our getted datas
-    obj = {"Sıra": 0, "Item": "", "Fiyat": "", "Listeleneceği saat": "", " -----------------------------> ": "✓"}
+    obj = {"Öğe": "", "Fiyat": "", "Listeleneceği saat": ""}
+    # converted variable for our messages
+    converted = str()
+    nonline = "\n" + "------------------------------------" + "\n" + "\n"
 
     # We querying if it's a list. Cuz theJSON has can more data
     if type(theJSON) == list:
@@ -18,19 +33,27 @@ def printResults(data):
             # now we can access the contents of the JSON like any other Python object
             for k,v in theJSON[i].items():
 
-                if k == "name":
-                    obj["Item"] = v
+                if k == "error":
+                    return("Wait list is empty.")
 
-                if k == "price":
-                    obj["Fiyat"] = "{:,d}".format(v)
+                else:
+                    if k == "name":
+                        obj["Öğe"] = v
 
-                if k == "liveAt":
-                    date = datetime.fromtimestamp(v).strftime("%H:%M:%S")
-                    obj["Listeleneceği saat"] = date
-                    obj["Sıra"] += 1
+                    if k == "price":
+                        obj["Fiyat"] = "{:,d}".format(v)
 
+                    if k == "liveAt":
+                        date = datetime.fromtimestamp(v).strftime("%H:%M:%S")
+                        obj["Listeleneceği saat"] = date
+
+            # this loop our values converting to string
             for key,val in obj.items():
-                print("""{} : {} """.format(key,val))
+                converted += key + " : " + str(val) + "\n"
+            converted += nonline
+        return(converted)
+        
+                    
 
     # if theJSON is not list, it has 1 data and it's dict
     else:
@@ -38,38 +61,36 @@ def printResults(data):
         # now we can access the contents of the JSON like any other Python object
         for k,v in theJSON.items():
 
-            if k == "name":
-                obj["Item"] = v
+            if k == "error":
+                    return("Wait list is empty.")
 
-            if k == "price":
-                obj["Fiyat"] = v
+            else:
+                if k == "name":
+                    obj["Öğe"] = v
 
-            if k == "liveAt":
-                date = datetime.fromtimestamp(v).strftime("%H:%M:%S")
-                obj["Listeleneceği saat"] = date
-                obj["Sıra"] += 1
+                if k == "price":
+                    obj["Fiyat"] = str("{:,d}".format(v))
 
+                if k == "liveAt":
+                    date = datetime.fromtimestamp(v).strftime("%H:%M:%S")
+                    obj["Listeleneceği saat"] = str(date)
+
+        # this loop our values converting to string
         for key,val in obj.items():
-            print("""{} : {}""".format(key,val))
+            converted += key + " : " + val + "\n"
+        return(converted)
         
 
-def WaitList():
-    # define a variable to hold the source URL
-    # in this case we'll use the free data feed from the arsha.io
-    # this feed lists Black Desert Online(MENA) Market Wait List
-    # if u want use this for other server just change 'YourServer' from "https://api.arsha.io/v2/YourServer/GetWorldMarketWaitList"
-    urlData = "https://api.arsha.io/v2/mena/GetWorldMarketWaitList"
 
-    # Open the URL and read the data
-    webUrl = urllib.request.urlopen(urlData)
-    if (webUrl.getcode() == 200):
-        data = webUrl.read().decode("utf-8")
-        printResults(data)
-    else:
-        print("Received an error from server, cannot retrieve results " +
-              str(webUrl.getcode()))
+# BOT EVENTS
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
+    if message.content.startswith('$waitlist'):
+        waitlist = WaitList()
+        await message.channel.send("```" +waitlist+"```")
+        
 
-if __name__ == "__main__":
-    WaitList()
-
+client.run('TOKEN')
