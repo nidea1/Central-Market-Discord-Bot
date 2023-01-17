8a7abb43d6773c52ab42e9c1fca1bbc231f0144c
Doruk ÇORALI <drk.corali@gmail.com>
2023-01-17 15:16:31 +0300

    Renamed bot file, updated bot and added new funcs

---
+++ bot.py
@@ -0,0 +1,106 @@
+import os
+import sys
+import random
+import platform
+import asyncio
+import json
+
+
+import discord
+from discord import Colour
+from discord.ext import commands, tasks
+from discord.ext.commands import Bot, Context
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if os.path.exists("config.json"):
+    config = json.load(open("config.json"))
+else:
+    sys.exit("config.json not found! Please add it and try again.")
+
+# if not os.path.isfile(f"{os.path.realpath(os.path.dirname(__file__))}/config.json"):
+#     sys.exit("'config.json' not found! Please add it and try again.")
+# else:
+#     with open(f"{os.path.realpath(os.path.dirname(__file__))}/config.json") as file:
+#         config = json.load(file)
+
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+intents.presences = True
+
+
+bot = Bot(command_prefix = "-", intents=intents, help_command=None)
+bot.config = config
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+    print(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+    print(f"Python version: {platform.python_version()}")
+    print(f"discord.py API version: {discord.__version__}")
+    print("-------------------")
+    status_task.start()
+
+@tasks.loop(minutes=1.0)
+async def status_task():
+    statuses = ["with you!", "with Discord!", "with humans!", "with Central Market!", "with Black Desert Online!"]
+    await bot.change_presence(activity=discord.Game(random.choice(statuses)))
+
+@bot.event
+async def on_message(message: discord.Message):
+    if message.author == bot.user or message.author.bot:
+        return
+    await bot.process_commands(message)
+
+@bot.event
+async def on_command_error(context: Context, error):
+
+    if isinstance(error, commands.CommandOnCooldown):
+        minutes, seconds = divmod(error.retry_after, 60)
+        hours, minutes = divmod(minutes, 60)
+        hours = hours % 24
+        embed = discord.Embed(
+            description=f"**Please slow down** - You can use this command again in {f'{round(hours)} hours' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} seconds' if round(seconds) > 0 else ''}.",
+            color= Colour.brand_red()
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, commands.MissingPermissions):
+        embed = discord.Embed(
+            description="You are missing the permission(s) `" + ", ".join(
+                error.missing_permissions) + "` to execute this command!",
+            color= Colour.brand_red()
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, commands.BotMissingPermissions):
+        embed = discord.Embed(
+            description="I am missing the permission(s) `" + ", ".join(
+                error.missing_permissions) + "` to fully perform this command!",
+            color= Colour.brand_red()
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, commands.MissingRequiredArgument):
+        embed = discord.Embed(
+            title="Error!",
+            description=str(error).capitalize(),
+            color= Colour.brand_red()
+        )
+        await context.send(embed=embed)
+    else:
+        raise error
+
+async def load_cogs():
+    for file in os.listdir("cogs"):
+        if file.endswith(".py"):
+            extension = file[:-3]
+            try:
+                await bot.load_extension(f"cogs.{extension}")
+                print(f"Loaded extension '{extension}'")
+            except Exception as e:
+                exception = f"{type(e).__name__}: {e}"
+                print(f"Failed to load extension {extension}\n{exception}")
+
+asyncio.run(load_cogs())
+bot.run(os.getenv("TOKEN"))
---
+++ cogs/general.py
@@ -0,0 +1,312 @@
+import os
+import sys
+import json
+import requests
+import itertools
+from datetime import datetime
+
+import discord
+from discord import Colour
+from discord.ext import commands, tasks
+from discord.ext.commands import Context
+
+
+class General(commands.Cog, name="general"):
+
+    def __init__(self, bot):
+        self.bot = bot
+        #DATA
+        if os.path.exists("data.json"):
+            self.data = json.load(open("data.json"))
+        else:
+            sys.exit("data.json not found! Please add it and try again.")
+
+        #STATICS
+        self.region_id = "region_id"
+        self.item_id = "item_id"
+        self.sid = "sid"
+        self.grade = "grade"
+        self.item_name = "item_name"
+        self.BTitle = "Merkez Pazar"
+        self.nonline = "\n" + "\n" + "------------------------------------" + "\n" + "\n" 
+        self.language = "tr"
+        self.server = "mena"
+        self.ITitle = self.grade + " " + self.item_name
+
+    def run(self):
+
+        #DYNAMICS
+        link = f"https://api.arsha.io/v2/{self.server}/orders?id={self.item_id}&sid={self.sid}&lang={self.language}"
+        response = requests.get(link)
+        self.theJSON = json.loads(response.text)
+        self.ITitle = self.grade + " " + self.item_name
+
+    def waitlistrun(self):
+        link = f"https://api.arsha.io/v2/{self.server}/GetWorldMarketWaitList?lang={self.language}"
+        response = requests.get(link)
+        self.theJSON = json.loads(response.text)
+
+    @commands.hybrid_command(
+        name="help",
+        description="help"
+    )
+    async def help(self, context: Context) -> None:
+        prefix = self.bot.config["prefix"]
+        embed = discord.Embed(
+            title="Help",
+            description="List of the available commands:",
+            color=Colour.blue())
+        for i in self.bot.cogs:
+            cog = self.bot.get_cog(i.lower())
+            commands = cog.get_commands()
+            comdata= []
+            for command in commands:
+                desc = command.description.partition('\n')[0]
+                comdata.append(f"{prefix}{command.name} - {desc}")
+            text = "\n".join(comdata)
+            embed.add_field(
+            name=i.capitalize(),
+            value=f"""```fix\n{text}```""",
+            inline=False)
+        await context.send(embed=embed)
+    
+    @commands.hybrid_command(
+        name="lang",
+        description="Central market language"
+    )
+    async def lang(self, context: Context, arg1 = "tr") -> None:
+        arg1 = arg1.lower()
+
+        if arg1 in self.data["languages"]:
+            self.language = arg1
+            embed = discord.Embed(
+                title=self.BTitle,
+                description="-lang :lang",
+                color=0x9C84EF)
+            embed.add_field(
+                name="Central Market Language",
+                value=f"""```arm\nCentral market language is {self.language.upper()} now.```""",
+                inline=False)
+        else:
+            embed = discord.Embed(
+                title=self.BTitle,
+                description="-lang :lang",
+                color=0x9C84EF)
+            embed.add_field(
+                name="Central Market Language",
+                value=f"""```arm\nYou did enter a wrong language.```""",
+                inline=False)
+
+        await context.send(embed=embed)
+
+    @commands.hybrid_command(
+    name="region",
+    description="Central market language"
+    )
+    async def region(self, context: Context, arg1 = "mena") -> None:
+        arg1 = arg1.lower()
+
+        if arg1 in self.data["regions"]:
+            self.server = arg1
+            embed = discord.Embed(
+                title=self.BTitle,
+                description="""```fix\n-region :region```""",
+                color=0x9C84EF)
+            embed.add_field(
+                name="Central Market Region",
+                value=f"""```fix\nCentral market region is {self.server.upper()} now.```""",
+                inline=False)
+        else:
+            embed = discord.Embed(
+                title=self.BTitle,
+                description="""```fix\n-region :region```""",
+                color=0x9C84EF)
+            embed.add_field(
+                name="Central Market Region",
+                value=f"""```fix\nYou did enter a wrong region.```""",
+                inline=False)
+
+        await context.send(embed=embed)
+
+
+
+    @commands.hybrid_command(
+    name="list",
+    description="Central Market item list.",
+    )
+    async def list(self, context: Context, arg1="", arg2="0") -> None:
+        arg1 = arg1.lower()
+        arg2 = arg2.lower()
+        
+        #ITEMS
+        for i in self.data["items"]:
+            if arg1 in i["search"]:
+                self.item_id = i["id"]
+                self.item_name = str(i["name"])
+                Ltxt = "Listing..."
+                break
+
+            elif arg1 == "help":
+                embed = discord.Embed(
+                    title=self.BTitle,
+                    description="""```fix\n-list :items :grades :regions```""",
+                    color=Colour.blue())
+                for i in dict(itertools.islice(self.data.items(), 4)):
+                    commands= []
+                    for x in self.data[i]:
+                        if type(x) == dict:
+                            commands.append(x["name"])
+                        else:
+                            commands.append(x)
+                    embed.add_field(
+                    name=i,
+                    value=f"""```fix\n{", ".join(map(str,commands))}```""",
+                    inline=False)
+
+            elif arg1 == "":
+                embed = discord.Embed(
+                    title=self.BTitle,
+                    description="""```fix\n-list :items :grades :regions```""",
+                    color=0x9C84EF)
+                embed.add_field(
+                    name=self.ITitle,
+                    value=f"""```fix\nYou did not enter a item.```""",
+                    inline=False)
+            else:
+                Ltxt = self.data["errors"][self.language][0]
+                self.item_name = str()
+
+        #GRADES
+        for k,v in self.data["grades"].items():
+            if arg2 in v:
+                self.grade = str(v[0]).upper()+":"
+                self.sid = k
+                Ltxt = "Listing..."
+                if arg2 == "0":
+                    self.grade = str(v[0]).upper()
+                    self.sid = k
+                    Ltxt = "Listing..."
+                    break
+                else:
+                    break
+            else:
+                Ltxt = self.data["errors"][self.language][1]
+                self.grade = ""
+
+        #RUN
+        if Ltxt in self.data["errors"][self.language] or arg1 == "":
+            pass
+        else:
+            self.run()
+            Ltxt = str()
+            Lsort = dict()
+            for i in range(len(self.theJSON["orders"])):
+                Lsort[i] = self.theJSON["orders"][i]
+
+            res = sorted(Lsort.items(), key = lambda x: x[1]['price'])
+            res.reverse()
+            res = dict(res)
+
+            embed = discord.Embed(
+                    title=self.BTitle,
+                    description=f"""```arm\nServer: {self.server.upper()}```""",
+                    color=0x9C84EF)
+            for x in res:
+                Ltxt = "Price: " + "{:,d}".format(res[x]["price"]) + "\n" + "Sellers: " + "{:,d}".format(res[x]["buyers"]) + "\n" + "Buyers: " + "{:,d}".format(res[x]["sellers"])
+                embed.add_field(
+                    name=self.ITitle if x == list(res)[0] else "",
+                    value=f"""```arm\n{Ltxt}```""",
+                    inline=False)
+
+
+                    
+
+        await context.send(embed=embed)
+
+    @commands.hybrid_command(
+        name="waitlist",
+        description = "Central Market in registration queue"
+    )
+    async def waitlist(self, context: Context):
+        self.waitlistrun()
+        WLtxt = str()
+        embed = discord.Embed(
+            title=self.BTitle,
+            description=f"""```arm\nServer: {self.server.upper()}```""",
+            color=0x9C84EF)
+
+        if type(self.theJSON) == list:
+            for i in range(len(self.theJSON)):
+                WLtxt = "Item: " + self.theJSON[i]["name"].replace("&#39;","") + "\n" + "Price: " + "{:,d}".format(self.theJSON[i]["price"]) + "\n" + "LiveAt: " + datetime.fromtimestamp(self.theJSON[i]["liveAt"]).strftime("%H:%M:%S")
+                embed.add_field(
+                    name="In registration queue" if i == 0 else "",
+                    value=f"""```arm\n{WLtxt}```""",
+                    inline=False)
+        else:
+            WLtxt = "Item: " + self.theJSON["name"].replace("&#39;","") + "\n" + "Price: " + "{:,d}".format(self.theJSON["price"]) + "\n" + "LiveAt: " + datetime.fromtimestamp(self.theJSON["liveAt"]).strftime("%H:%M:%S")
+            embed.add_field(
+                name="In registration queue",
+                value=f"""```arm\n{WLtxt}```""",
+                inline=False)
+        
+        
+
+        await context.send(embed=embed, delete_after=15)
+
+    @commands.hybrid_command(
+        name="loop",
+        description = "Central Market in registration queue loop"
+    )
+    async def loop(self, context: Context, arg1=""):
+        arg1 = arg1.lower()
+        
+        if arg1 == "":
+            embed = discord.Embed(
+                title=self.BTitle,
+                description="""```fix\n-loop help | start | stop region```""",
+                color=0x9C84EF)
+            embed.add_field(
+                name="In registration queue",
+                value=f"""```fix\nYou did not enter a command.```""",
+                inline=False)
+        
+        elif arg1 == "help":
+            embed = discord.Embed(
+                title=self.BTitle,
+                description="""```fix\n-loop start | stop region```""",
+                color=0x9C84EF)
+            embed.add_field(
+                name="In registration queue",
+                value=f"""```fix\nThis loop is replacing every 15 seconds.```""",
+                inline=False)
+            
+
+        elif arg1 == "start":
+            self.WaitListLoop.start(context)
+
+        elif arg1 == "stop":
+            self.WaitListLoop.cancel()
+
+        else:
+            embed = discord.Embed(
+                title=self.BTitle,
+                description="""```fix\n-loop help | start | stop region```""",
+                color=0x9C84EF)
+            embed.add_field(
+                name="In registration queue",
+                value=f"""```fix\nYou did enter a wrong command.```""",
+                inline=False)
+
+        await context.send(embed=embed)
+
+
+    @tasks.loop(seconds= 15)
+    async def WaitListLoop(self, context : Context):
+        await self.waitlist(context)
+
+
+
+
+
+async def setup(bot):
+    await bot.add_cog(General(bot))
